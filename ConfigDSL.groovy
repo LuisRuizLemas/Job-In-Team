8aa755920a3efa4a0365f734959cf2e00cd29dce
Luis Gregorio Ruiz Lemas <97902495+LuisRuizLemas@users.noreply.github.com>
2022-04-05 22:22:24 -0700

    Update ConfigDSL.groovy

--- ConfigDSL.groovy
+++ ConfigDSL.groovy
@@ -8,8 +8,8 @@ job('Avanzado-job-DSL') {
     	} 
   	parameters {
                 stringParam('nombre', defaultValue = 'Luis', description = 'Parametro de cadena para el Job Estudiante')
-                stringParam('n1', defaultValue = '0', description = 'Parametro de cadena para el numero 1')
-                stringParam('n2', defaultValue = '0', description = 'Parametro de cadena para el numero 2')
+                stringParam('n1', defaultValue = '1', description = 'Parametro de cadena para el numero 1')
+                stringParam('n2', defaultValue = '1', description = 'Parametro de cadena para el numero 2')
       		choiceParam('materia', ['Aritmetica', 'Operaciones Basicas'])
                 choiceParam('idioma', ['Español', 'Ingles', 'Japones', 'Latin', 'Frances'])
       		booleanParam('Estudiante_Avanzado', false)
